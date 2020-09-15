fcbf1c6e07057cd6c2df5f0a734a67ab425954af
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-09-15 13:54:28 +1000

    creating separate CSS files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+// import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -17,8 +17,8 @@ store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
 	<Provider store={store}>
